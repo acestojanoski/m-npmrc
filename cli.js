2eb5718a2935affe86b23d01cfa2a3898f2f95a8
Aleksandar Stojanoski <aleksandar-stojanoski@hotmail.com>
2020-02-26 18:24:37 +0100

    sync readme and help text

--- cli.js
+++ cli.js
@@ -17,24 +17,24 @@ const homeDirectory = os.homedir();
 const configurationsDirectory = path.join(os.tmpdir(), 'm-npmrc');
 
 const cli = argsFlagify(`
-    Usage
-      $ m-npmrc <input>
+	Usage
+	  $ m-npmrc <input>
 
-    Commands
-      new (create a new npmrc configuration)
-      use (use an existing npmrc configuration)
-      list (list your npmrc configurations)
-      remove (remove an existing npmrc configuration)
+	Commands
+	  new (create a new npmrc configuration)
+	  use (use an existing npmrc configuration)
+	  list (list your npmrc configurations)
+	  remove (remove an existing npmrc configuration)
 
-    Flags
-      --version (Display the CLI version)
-      --help (Display the help text)
+	Flags
+	  --version (display the m-npmrc version)
+	  --help (display the help text)
 
-    Examples
-      $ m-npmrc new
-      $ m-npmrc use
-      $ m-npmrc list
-      $ m-npmrc remove
+	Examples
+	  $ m-npmrc new
+	  $ m-npmrc use
+	  $ m-npmrc list
+	  $ m-npmrc remove
 `);
 
 if (!fs.existsSync(configurationsDirectory)) {
